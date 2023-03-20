7d1032eb481f8ccf1f76b6ad0a61be3b01287cef
B00TTERR0R <35654020+frek666@users.noreply.github.com>
2023-03-20 11:25:20 +0300

    Update membot.py

--- membot.py
+++ membot.py
@@ -3,18 +3,18 @@ import time
 
 # Creating an instance of the bot
 bot = telebot.TeleBot('Here enter the token received from @botfather')
-# b
+# the channel address starts with @
 CHANNEL_NAME = '@the_address_of_your_channel'
 
-# c
+# uploading memes
 f = open('data/fun.txt', 'r', encoding='UTF-8')
 mem = f.read().split('\n')
 f.close()
 
-# d
+# until the memes run out, we send them to the chat
 for mem in mems:
     bot.send_message(CHANNEL_NAME, joke)
-    # e
+    # pause at 1 hour
     time.sleep(3600)
 
 bot.send_message(CHANNEL_NAME, "MEM IS OVER :-(")
